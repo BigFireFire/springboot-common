a837313f4979be48ea0fc0ae6597d0bfacfc16f0
楼中煊 <64390357@qq.com>
2020-05-09 10:24:00 +0800

    修改全局错误返回信息

--- core/src/main/java/com/itactic/core/config/ExceptionConfig.java
+++ core/src/main/java/com/itactic/core/config/ExceptionConfig.java
@@ -4,10 +4,10 @@ import com.itactic.core.exception.BootCustomException;
 import com.itactic.core.model.AjaxResult;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class ExceptionConfig {
     private static final Logger logger = LoggerFactory.getLogger(ExceptionConfig.class);
 
@@ -15,10 +15,11 @@ public class ExceptionConfig {
     public AjaxResult<String> handleException(Exception e) {
         if (e instanceof BootCustomException) {
             BootCustomException bootCustomException = (BootCustomException) e;
-            return AjaxResult.error(bootCustomException.getMessage());
+            logger.error("【BootCustomException】：{}", bootCustomException.getMessage());
+            return AjaxResult.error("系统错误");
         } else {
             logger.error("【系统异常】{}", e.getMessage());
-            return AjaxResult.error(e.getMessage());
+            return AjaxResult.error("系统错误");
         }
     }
 }
