18a578c53fd0c6d1a5f1640bee4eb79bd27e1082
louzx <64390357@qq.com>
2020-05-13 11:27:36 +0800

    新增NoHandlerFoundException

--- core/src/main/java/com/itactic/core/config/ExceptionConfig.java
+++ core/src/main/java/com/itactic/core/config/ExceptionConfig.java
@@ -4,21 +4,27 @@ import com.itactic.core.exception.BootCustomException;
 import com.itactic.core.model.AjaxResult;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
-@RestControllerAdvice
+@ControllerAdvice
 public class ExceptionConfig {
     private static final Logger logger = LoggerFactory.getLogger(ExceptionConfig.class);
 
+    @ResponseBody
     @ExceptionHandler(value = Exception.class)
     public AjaxResult<String> handleException(Exception e) {
         if (e instanceof BootCustomException) {
             BootCustomException bootCustomException = (BootCustomException) e;
-            logger.error("【BootCustomException】：{}", bootCustomException.getMessage());
+            logger.error("【BootCustomException】:{}", bootCustomException.getMessage());
             return AjaxResult.error("系统错误");
-        } else {
-            logger.error("【系统异常】{}", e.getMessage());
+        } else if (e instanceof NoHandlerFoundException) {
+            logger.error("【系统异常】:{}", e.getMessage());
+            return AjaxResult.error("接口不存在");
+        } else  {
+            logger.error("【系统异常】:{}", e.getMessage());
             return AjaxResult.error("系统错误");
         }
     }
