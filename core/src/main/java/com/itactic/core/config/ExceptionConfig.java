7e5743b9c301a870847ce468efd6ee9dffa3559b
BigFF <linyh5@asiainfo.com>
2020-05-17 19:54:01 +0800

    增加全局异常处理

--- core/src/main/java/com/itactic/core/config/ExceptionConfig.java
+++ core/src/main/java/com/itactic/core/config/ExceptionConfig.java
@@ -4,6 +4,7 @@ import com.itactic.core.exception.BootCustomException;
 import com.itactic.core.model.AjaxResult;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -27,7 +28,10 @@ public class ExceptionConfig {
         } else if (e instanceof MissingServletRequestParameterException) {
             logger.error("【系统异常】:{}", e.getMessage());
             return AjaxResult.error("缺少必填参数");
-        } else  {
+        } else if( e instanceof HttpMessageNotReadableException){
+            logger.error("【系统异常】:{}", e.getMessage());
+            return AjaxResult.error("请求参数不合法");
+        }else  {
             logger.error("【系统异常】:{}", e.getMessage());
             return AjaxResult.error("系统错误");
         }
