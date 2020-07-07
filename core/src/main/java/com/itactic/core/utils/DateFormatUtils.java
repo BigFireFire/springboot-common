c4258467df413242b67c9416ede798853f02e1a0
louzx <64390357@qq.com>
2020-07-07 11:40:18 +0800

    新增时间转化工具

---
+++ core/src/main/java/com/itactic/core/utils/DateFormatUtils.java
@@ -0,0 +1,40 @@
+package com.itactic.core.utils;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+
+/**
+ * @author 1Zx.
+ * @date 2020/7/7 11:00
+ */
+@Component
+public class DateFormatUtils {
+
+    private static String pattern;
+
+    private static DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern(pattern);
+
+    public static Date format (String timeMillis) {
+        if (StringUtils.isBlank(timeMillis) || !StringUtils.isNumeric(timeMillis) || timeMillis.length() != 13) {
+            return null;
+        }
+        return new Date(Long.valueOf(timeMillis));
+    }
+
+    public static String format (Date date) {
+        LocalDateTime localDateTime = date.toInstant().atZone(ZoneId.of("Asia/Shanghai")).toLocalDateTime();
+        return dateTimeFormatter.format(localDateTime);
+    }
+
+    @Value("date.format.pattern:'yyyy-MM-dd HH:mm:ss'")
+    public void setDateTimeFormatter(DateTimeFormatter dateTimeFormatter) {
+        DateFormatUtils.dateTimeFormatter = dateTimeFormatter;
+    }
+
+}
