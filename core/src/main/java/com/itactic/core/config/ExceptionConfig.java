f48302a8a1d1c3c1b15a8816b08ff42c8de4473f
louzx <64390357@qq.com>
2020-12-03 16:37:32 +0800

    调整全局异常捕获

--- core/src/main/java/com/itactic/core/config/ExceptionConfig.java
+++ core/src/main/java/com/itactic/core/config/ExceptionConfig.java
@@ -13,27 +13,27 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import org.springframework.web.servlet.NoHandlerFoundException;
 
+import javax.servlet.http.HttpServletRequest;
+
 @ControllerAdvice
 public class ExceptionConfig {
     private static final Logger logger = LoggerFactory.getLogger(ExceptionConfig.class);
 
     @ResponseBody
     @ExceptionHandler(value = Exception.class)
-    public AjaxResult<String> handleException(Exception e) {
-        logger.error("【系统异常】:{}", e.getMessage());
-        if (e instanceof BootCustomException) {
-            return AjaxResult.error("系统错误");
+    public AjaxResult<String> handleException(HttpServletRequest request, Exception e) {
+        logger.error("【系统异常】：请求路径：【{}】，错误信息：【{}】",request.getRequestURI(), e.getMessage());
+        if (e instanceof BootCustomException || e instanceof ImportException) {
+            return AjaxResult.error(e.getMessage());
         } else if (e instanceof NoHandlerFoundException) {
             return AjaxResult.error("接口不存在");
         } else if (e instanceof MissingServletRequestParameterException) {
-            return AjaxResult.error("缺少必填参数");
+            return AjaxResult.error(String.format("缺少【%s】参数，参数类型：【%s】",((MissingServletRequestParameterException) e).getParameterName(), ((MissingServletRequestParameterException) e).getParameterType() ));
         } else if ( e instanceof HttpMessageNotReadableException) {
-            return AjaxResult.error("请求参数不合法");
+            return AjaxResult.error("请求体不合法");
         } else if ( e instanceof MethodArgumentTypeMismatchException) {
             return AjaxResult.error("参数类型不合法");
-        } else if (e instanceof ImportException){
-            return AjaxResult.error("导入失败");
-        }else{
+        } else {
             return AjaxResult.error("系统错误");
         }
     }
